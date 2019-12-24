b3d0e94f8e8cf88ce657db52d840bb784992f887
yevheniiiiii <yevgenii.andrieiev@innovecs.com>
2019-12-24 19:40:24 +0200

    started to build angular project based on official doc

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { HeroService } from "../services/hero.service";
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+
+@Component({
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.scss"]
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => {
+      this.heroes = heroes;
+    });
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
